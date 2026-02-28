8a847a3c1f647bbb1260458b7d7070716bae3281
mariam-lumin <mariam.ismail@luminsoft.net>
2026-02-28 16:32:15 +0200

    Version 1.0.3

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -22,9 +22,9 @@ class _MyAppState extends State<MyApp> {
       return Scaffold(
         body: EnrollNeoPlugin(
           mainScreenContext: context,
-          tenantId: '3489aa92-46f7-4e6e-a0f8-369083a6fb03',
-          tenantSecret: '83d7afe4-42f5-4438-a6ba-5e38d43af4fc',
-          requestId: null,
+          tenantId: 'TENANT_ID',
+          tenantSecret: 'TENANT_SECRET',
+          requestId:'requestId',
           enrollMode: EnrollMode.onboarding,
           enrollEnvironment: EnrollEnvironment.production,
           onSuccess: (applicantId) {
